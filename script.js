3f3b4d906ec04924de950bb32145a6ca336df88a
Ansanat <Ansanat228@gmail.com>
2023-06-08 17:34:51 +0700

    add brootstrap

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function mobileMenu() {
     document.getElementById("mobile_menu").style.display = 'none';
     document.getElementById("main_menu").style.display = 'block';
 }
-
-function cancelIconClick() {
-    document.getElementById("mobile_menu").style.display = 'block';
-    document.getElementById("main_menu").style.display = 'none';
-}
